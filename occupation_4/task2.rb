8d7605836fbb0754ebf88650ae4e7cc1447b6539
kayrat <kairatomurbek2@gmail.com>
2015-10-04 23:15:12 +0600

    Прошел Задание 2

---
+++ occupation_4/task2.rb
@@ -0,0 +1,8 @@
+first_array_numbers = [1, 2, 3, 4, 5]
+second_array_numbers = [3, 4, 5, 6, 7, 7]
+
+first_array_strings = ["table", "chair", "door", "glass", "pencil", "scissors"]
+second_array_strings = ["chair", "snake", "dog", "table", "cat", "elephant"]
+
+puts first_array_numbers & second_array_numbers
+puts first_array_strings & second_array_strings
